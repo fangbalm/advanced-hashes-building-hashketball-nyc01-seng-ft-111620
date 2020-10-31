33f8cc3d32077339228aae43e0ed3a1d7e18539b
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-31 02:08:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -57,8 +57,8 @@ def game_hash
       ]
     },
     :away => {
-      :team_name => [],
-      :colors => [],
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
       :players => []
     }
   }
